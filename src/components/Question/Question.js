6f4455ec06596150660178a6c9270975e0974a8c
antudas22 <20antu20@gmail.com>
2022-10-12 05:23:21 +0600

    Toast part added

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './Question.css'
 
 const Question = ({myqna}) => {
     const {question, options, correctAnswer} = myqna;
     const myQuestion = (question).slice(3, -4)
     console.log(myqna)
+
+    const checkCorrectOrWrong = (option) => {
+        if(correctAnswer === option){
+            toast.success('Correct Answer!', {autoClose: 1000})
+        }
+        else{
+            toast.warning('Wrong Answer!', {autoClose: 1000})
+        }
+    }
+
     return (
-        <div>
-                <h3>{myQuestion}</h3>
-                <div>
-                    {
-                        options.map(option => <p>{option}</p>)
-                    }
+        <div className='questions-container'>
+                <div className='question'>
+                    <h3>Q: {myQuestion}</h3>
+                    <div className='options'>
+                        {
+                            options.map(option => <button className='option' onClick={() => checkCorrectOrWrong(option)}><p>{option}</p></button>)
+                        }
+                    </div>
                 </div>
+                <ToastContainer />
         </div>
     );
 };
