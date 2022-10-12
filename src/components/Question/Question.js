52e22b12d4bce896f3dd1f01b5df98698a014a80
antudas22 <20antu20@gmail.com>
2022-10-12 14:15:14 +0600

    show correctAns part completed

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './Question.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faEye } from '@fortawesome/free-solid-svg-icons'
 
 const Question = ({myqna}) => {
     const {question, options, correctAnswer} = myqna;
-    const myQuestion = (question).slice(3, -4)
-    console.log(myqna)
+    const myQuestion = (question).slice(3, -4);
+
+    const [ans, setAns] = useState();
+
+    const showCorrectAns = (correctAnswer) => {
+        setAns(correctAnswer);
+    }
 
     const checkCorrectOrWrong = (option) => {
         if(correctAnswer === option){
@@ -20,11 +27,17 @@ const Question = ({myqna}) => {
     return (
         <div className='questions-container'>
                 <div className='question'>
-                    <h3>Q: {myQuestion}</h3>
+                    <div className='question-header'>
+                        <div><h3>Q: {myQuestion}</h3></div> 
+                        <div><button onClick={() => showCorrectAns(correctAnswer)}><FontAwesomeIcon icon={faEye}></FontAwesomeIcon></button></div>
+                    </div>
                     <div className='options'>
                         {
                             options.map(option => <button className='option' onClick={() => checkCorrectOrWrong(option)}><p>{option}</p></button>)
                         }
+                    </div>
+                    <div className='show'>
+                        <h3>Correct Answer is: {ans}</h3>
                     </div>
                 </div>
                 <ToastContainer />
